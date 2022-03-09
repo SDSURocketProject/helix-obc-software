253826b446412afaf32c8a3f60407cb3036763ba
Jvives7 <jeanmichelvs@gmail.com>
2022-03-09 12:44:05 -0800

    adding TCP library file, still needs work

--- OBC_CAN/main.cpp
+++ OBC_CAN/main.cpp
@@ -9,11 +9,42 @@
 // via the Linux socketCAN interface
 //
 
-#include <iostream>
+// used http://www.microhowto.info/howto/get_the_index_number_of_a_linux_network_interface_in_c_using_siocgifindex.html
 
-// create a socket
-int socket;
 
-if ((socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+#include <can.h> // for can_frame
+#include <linux/can.h>
+
+//    Opens A Socket and Bind
+
+// opens socket with protocolamily can, raw socket data, and bind can_raw
+int socket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+
+// checks if there is an error
+if (socket < 0) {
     perror("Socket");
 }
+
+// 1) Create ifreq sturct for passing data in and out of struct
+struct ifreq ifr;
+
+// 2) strcpy copies c string pointed by source into the array pointed by destination
+// strcpy(dest, source) source into destination
+strcpy(ifr.ifr_name, "vcan0");
+
+// 3) Invoke ioctl (in out control)
+// index # of network interface obtained using ioctl command SIOCGIFINDEX
+// SIOCGIFINDEX = Given the ifr_ifindex, return the name of the interface in ifr_name.
+ioctl (socket, SIOCGIFINDEX, &ifr);
+
+
+
+//    Reading A Frame
+
+// to recieve a CAN frame, init can_frame struct and populate with data
+struct can_frame frame;
+
+// reads sizeof(frame) bytes from socket into buffer pointed by &frame
+nbytes = read(socket, &frame, sizeof(frame));
+
+frame
---
+++ OBC_TCP/logging_lib.py
@@ -0,0 +1,2 @@
+# going to add logging functions library here to be included into the BeagleBone black
+# TCP server
