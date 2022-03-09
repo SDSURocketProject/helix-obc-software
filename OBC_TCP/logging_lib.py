5e4c5735b89abb42669ce2a4e14515b0e1b75132
Jvives7 <jeanmichelvs@gmail.com>
2022-03-09 12:45:11 -0800

    adding TCP library file, still needs work

--- OBC_TCP/logging_lib.py
+++ OBC_TCP/logging_lib.py
@@ -1,2 +1,4 @@
 # going to add logging functions library here to be included into the BeagleBone black
 # TCP server
+
+# look into this tutorial https://projects.raspberrypi.org/en/projects/temperature-log/2
